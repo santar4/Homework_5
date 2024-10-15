9a645175394f064b3e776d285478e1b934397695
santar4 <mdykovv@gmail.com>
2024-10-15 15:31:47 +0300

    homework5

---
+++ main.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+from typing import Optional
+
+import uvicorn
+
+from fastapi import FastAPI, HTTPException, Response, Query, Path, Header
+from starlette.responses import RedirectResponse
+
+app = FastAPI(debug=True)
+
+@app.get('/', include_in_schema=False)
+def root():
+    return RedirectResponse("/docs")
+
+
+@app.get('/user_info/{user_id}/')
+def find_user(
+        user_id: int = Path(..., description="Ідентифікатор користувача"),
+        timestamp: Optional[str] = Query(None, description="Мітка часу"),
+        x_client_version: str = Header(..., alias="X-Client-Version")
+):
+    if not timestamp:
+        timestamp = datetime.now(timezone.utc).astimezone().isoformat()
+
+    return {
+        "Welcome": f"Hello, user {user_id}!",
+        "user_id": user_id,
+        "timestamp": timestamp,
+        "X-Client-Version": x_client_version
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
